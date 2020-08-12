9c71d5871f3060fc25f13267276d78fda8b58ebb
Anton <a.u.lysenko@gmail.com>
2020-08-13 00:09:33 +0300

    updated

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,9 @@ import './styles/styles.scss';
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
-
+// TODO update app
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<Routes />, document.getElementById('root'));
 serviceWorker.unregister();
